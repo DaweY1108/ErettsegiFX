cbe139923afb1666e5e56871bf0b48b4731469a7
DaweY1108 <vargadavidzsolt@gmail.com>
2024-10-07 20:30:12 +0200

    :soap: Soap is working with jdk 17

--- src/main/java/me/dawey/erettsegifx/Main.java
+++ src/main/java/me/dawey/erettsegifx/Main.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import me.dawey.erettsegifx.database.Database;
 import me.dawey.erettsegifx.database.tables.Vizsgazo;
+import soap.MNBArfolyamServiceSoap;
+import soap.MNBArfolyamServiceSoapImpl;
 
 import java.io.IOException;
 import java.util.List;
@@ -30,11 +32,26 @@ public class Main extends Application {
         }
 
 
+        MNBArfolyamServiceSoapImpl impl = new MNBArfolyamServiceSoapImpl();
+        MNBArfolyamServiceSoap service = impl.getCustomBindingMNBArfolyamServiceSoap();
+        try {
+            System.out.println(service.getInfo());
+            System.out.println(service.getCurrentExchangeRates());
+            System.out.println(service.getExchangeRates("2022-08-14","2022-09-14","EUR"));
+        } catch (Exception e) {
+            System.err.print(e);
+        }
+
+
         System.out.println("Az app sikeresen elindult!");
     }
 
     //App entry point
     public static void main(String[] args) {
+        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.util.pipe.StandaloneTubeAssembler.dump", "true");
+
         launch();
     }
 }
