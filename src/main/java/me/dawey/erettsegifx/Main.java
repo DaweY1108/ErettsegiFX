65d4abc0c7bbab3632aedbc3cc51e4a31de26047
DaweY1108 <vargadavidzsolt@gmail.com>
2024-10-08 14:38:52 +0200

    :white_check_mark: Added more functions to oanda

--- src/main/java/me/dawey/erettsegifx/Main.java
+++ src/main/java/me/dawey/erettsegifx/Main.java
@@ -1,22 +1,19 @@
 package me.dawey.erettsegifx;
 
+import com.j256.ormlite.logger.Level;
+import com.j256.ormlite.logger.Logger;
 import com.oanda.v20.account.AccountSummary;
+import com.oanda.v20.instrument.InstrumentCandlesResponse;
+import com.oanda.v20.pricing.PricingGetResponse;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import me.dawey.erettsegifx.models.database.Database;
-import me.dawey.erettsegifx.models.database.tables.Vizsgazo;
-import me.dawey.erettsegifx.models.mnbank.BankManager;
-import me.dawey.erettsegifx.models.mnbank.data.Day;
-import me.dawey.erettsegifx.models.mnbank.data.ExchangeData;
-import me.dawey.erettsegifx.models.mnbank.data.MNBExchangeRates;
-import me.dawey.erettsegifx.models.mnbank.data.Rate;
 import me.dawey.erettsegifx.models.oanda.Oanda;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Main extends Application {
 
@@ -29,43 +26,15 @@ public class Main extends Application {
         stage.setTitle("ErettsegiFX");
         stage.setScene(scene);
         stage.show();
-        Database database = new Database();
 
-        List<Vizsgazo> vizsgazok = database.getAllVizsgazok();
-        for (Vizsgazo vizsgazo : vizsgazok) {
-            System.out.println(vizsgazo.getNev());
-        }
-
-        /*
-        BankManager bankManager = new BankManager();
-        List<ExchangeData> exchangeDataList = bankManager.getExchangeDatas("2022-08-14", "2022-09-14", "EUR");
-        for (ExchangeData exchangeData : exchangeDataList) {
-            System.out.println("Date: " + exchangeData.getDate());
-            System.out.println("Currency: " + exchangeData.getCurrency());
-            System.out.println("Unit: " + exchangeData.getUnit());
-            System.out.println("Value: " + exchangeData.getValue() + "\n");
-
-        }
-        */
-
-        Oanda oanda = new Oanda();
-        AccountSummary summary = oanda.getSummary();
-        System.out.println("Account ID: " + summary.getId());
-        System.out.println("Account Alias: " + summary.getAlias());
-        System.out.println("Account Balance: " + summary.getBalance());
-        System.out.println("Account Currency: " + summary.getCurrency());
-        System.out.println("Account Created By: " + summary.getCreatedByUserID());
-        System.out.println("Account Created Time: " + summary.getCreatedTime());
-
+        System.out.println("Adatbazis inicializalasa...");
+            Database database = new Database();
+        System.out.println("Adatbazis inicializalva!");
         System.out.println("Az app sikeresen elindult!");
     }
 
-    //App entry point
     public static void main(String[] args) {
-        //System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        //System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        //System.setProperty("com.sun.xml.ws.util.pipe.StandaloneTubeAssembler.dump", "true");
-
+        Logger.setGlobalLogLevel(Level.ERROR);
         launch();
     }
 }
